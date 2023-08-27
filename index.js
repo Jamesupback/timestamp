ee026b7c97b0f6fd789ed13ef03bcf9ab673d510
Replit user <>
2023-08-27 17:18:40 +0000

    done

--- index.js
+++ index.js
@@ -20,12 +20,29 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api", function (req, res) {
+  date=new Date()
+  res.json({"unix":date.getTime(),"utc":date.toUTCString()});
+})
 
-
-
+app.get("/api/:date",(req,res)=>{
+  unix=/\d{13}/;
+  date=new Date(req.params.date);
+  if(unix.test(req.params.date))
+  {
+    udate=new Date(Number(req.params.date))
+    res.json({"unix":udate.getTime(),"utc":udate.toUTCString()});
+  }
+  else if(date instanceof Date && !isNaN(date))
+  {
+    res.json({"unix":date.getTime(),"utc":date.toUTCString()});
+  }
+  else
+  {
+    res.json({ "error" : "Invalid Date" })
+  }
+  
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
